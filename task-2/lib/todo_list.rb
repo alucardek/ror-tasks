790ef914e53c29d4697acf26e99e48fc7872eacd
Aleksander Kropidlowski <kropidlowski.aleksander@gmail.com>
2013-04-10 03:59:01 +0200

    task2 almost there

--- task-2/lib/todo_list.rb
+++ task-2/lib/todo_list.rb
@@ -4,6 +4,7 @@ class TodoList
   def initialize(options={})
     raise(IllegalArgument) unless options[:db]
     @db=options[:db]
+    @socl=options[:socl]
   end
 
   def empty?
@@ -15,7 +16,11 @@ class TodoList
   end
 
   def << (item)
-    @db.add_todo_item(item) unless item.title.empty? || item.title.nil? || item.nil?
+    unless item.nil? || item.title.nil? || item.title.empty? || item.title.length < 5
+      @db.add_todo_item(item)
+      message = (item.description.nil? ? nil : item.description )
+      @socl.spam(message) if @socl
+    end
   end
 
   def first
@@ -27,11 +32,20 @@ class TodoList
   end
 
   def toggle_state(index)
-    if @db.get_todo_item(index)
-      @db.complete_todo_item(index, !@db.todo_item_completed?(index))
+    item=@db.get_todo_item(index)
+    unless item.nil?
+      #@db.complete_todo_item(index, !@db.todo_item_completed?(index))  <= szybsze rozwiązanie gdy nie ma socl
+      unless @db.todo_item_completed?(index)
+        @db.complete_todo_item(index, true)
+        message = item.description + " - completed!" unless item.description.nil?
+        @socl.spam( message ) if @socl
+      else
+        @db.complete_todo_item(index, false)
+      end
     else
       raise IllegalArgument
     end
   end
+
 
 end
--- task-2/spec/todo_spec.rb
+++ task-2/spec/todo_spec.rb
@@ -14,77 +14,90 @@ describe TodoList do
     expect{ TodoList.new(db: nil) }.to raise_error(IllegalArgument)
   end
 
-  it "should be empty if there are no items in the DB" do
-    stub(database).items_count { 0 }
-    list.should be_empty
+  context "with empty DB" do
+
+    it "should be empty if there are no items in the DB" do
+      stub(database).items_count { 0 }
+      list.should be_empty
+    end
+
+    it "should return nil for the first and the last item if the DB is empty" do
+      stub(database).items_count { 0 }
+
+      list.last.should == nil
+      list.first.should == nil
+    end
+
   end
 
-  it "should not be empty if there are some items in the DB" do
-    stub(database).items_count { 1 }
-    list.should_not be_empty
-  end
+  context "with some items in DB" do
 
-  it "should return its size" do
-    stub(database).items_count { 6 }
-    list.size.should == 6
-  end
+    it "should not be empty if there are some items in the DB" do
+      stub(database).items_count { 1 }
+      list.should_not be_empty
+    end
 
-  it "should persist the added item" do
-    mock(database).add_todo_item(item) { true }
-    mock(database).get_todo_item(0) { item }
-    mock(database).get_todo_item(0) { item }
-    stub(database).items_count { 1 }
+    it "should return its size" do
+      stub(database).items_count { 6 }
+      list.size.should == 6
+    end
 
-    list << item
-    list.first.should == item
-    list.last.should == item
-  end
+    it "should fetch the first item from the DB" do
+      stub(database).items_count {6 }
 
-  it "should persist the state of the item" do
-    mock(database).todo_item_completed?(0) { false }
-    mock(database).complete_todo_item(0,true) { true }
-    mock(database).todo_item_completed?(0) { true }
-    mock(database).complete_todo_item(0,false) { true }
+      mock(database).get_todo_item(0) { item }
+      list.first.should == item
 
-    list.toggle_state(0)
-    list.toggle_state(0)
-  end
+      mock(database).get_todo_item(0) { nil }
+      list.first.should == nil
+    end
 
-  it "should fetch the first item from the DB" do
-    mock(database).get_todo_item(0) { item }
-    list.first.should == item
+    it "should fetch the last item from the DB" do
+      stub(database).items_count { 6 }
 
-    mock(database).get_todo_item(0) { nil }
-    list.first.should == nil
-  end
+      mock(database).get_todo_item(5) { item }
+      list.last.should == item
 
-  it "should fetch the last item from the DB" do
-    stub(database).items_count { 6 }
+      mock(database).get_todo_item(5) { nil }
+      list.last.should == nil
+    end
 
-    mock(database).get_todo_item(5) { item }
-    list.last.should == item
+    it "should persist the added item" do
+      mock(database).add_todo_item(item) { true }
+      mock(database).get_todo_item(0).times(2) { item } # dwa razy bo sprawdzam dla first i dla last
+      stub(database).items_count { 1 }
 
-    mock(database).get_todo_item(5) { nil }
-    list.last.should == nil
-  end
+      list << item
+      list.first.should == item
+      list.last.should == item
+    end
 
-  it "should return nil for the first and the last item if the DB is empty" do
-    stub(database).items_count { 0 }
+    it "should not accepting a nil item" do
+      stub(database).items_count { 0 }
+      dont_allow(database).add_todo_item(nil)
 
-    list.last.should == nil
-    list.first.should == nil
-  end
+      list << nil
+      list.should be_empty
+    end
 
-  it "should raise an exception when changing the item state if the item is nil" do
-    mock(database).get_todo_item(0) {nil}
+    it "should persist the state of the item" do
+      mock(database).todo_item_completed?(0) { false }
+      mock(database).complete_todo_item(0,true) { true }
+      mock(database).todo_item_completed?(0) { true }
+      mock(database).complete_todo_item(0,false) { true }
 
-    expect{ list.toggle_state(0) }.to raise_error(IllegalArgument)
-  end
+      mock(database).get_todo_item(0).times(2) { item }
 
-  it "should not accepting a nil item" do
-    dont_allow(database).add_todo_item(nil)
+      list.toggle_state(0)
+      list.toggle_state(0)
+    end
 
-    list << item
+    it "should raise an exception when changing the item state if the item is nil" do
+      mock(database).get_todo_item(0) {nil}
+
+      expect{ list.toggle_state(0) }.to raise_error(IllegalArgument)
+    end
+
   end
 
   context "with empty title of the item" do
@@ -95,6 +108,99 @@ describe TodoList do
 
       list << item
     end
+  end
+
+  context "with short title" do
+    let(:title)   { "OMG" }
+
+    it "should not accept an item with too short (but not empty) title" do
+      dont_allow(database).add_todo_item(item)
+
+      list << item
+    end
+  end
+
+  context "with missing description" do
+    let(:description) {nil}
+
+    it "should accept of an item with missing description" do
+      mock(database).add_todo_item(item) { true }
+      mock(database).get_todo_item(0).times(2) { item } # dwa razy bo sprawdzam dla first i dla last
+      stub(database).items_count { 1 }
+
+      list << item
+      list.first.should == item
+      list.last.should == item
+    end
+
+  end
+
+  context "with social network" do
+    subject(:list)            { TodoList.new(db: database, socl: social_network) }
+    #let(:social_network)      { mock!.spam(description) { true }.subject }
+    let(:social_network)      { mock } #ALTERNATYWNE PODEJŚCIE - które lepsze?
+    let(:social_sufix)        { "- completed!"}
+
+    it "should notify a social network if an item is added to the list" do
+      mock(database).add_todo_item( item )  {true}
+      mock(social_network).spam(description) {true} # ALTERNATYWNE PODEJŚCIE - które lepsze?
+
+      list << item
+    end
+
+    it "should notify a social network if an item is completed" do
+      mock(database).add_todo_item( item ) {true}
+      mock(social_network).spam(description) {true}  # ALTERNATYWNE PODEJŚCIE - które lepsze?
+
+      mock(database).get_todo_item (0) {item}
+      mock(database).todo_item_completed?(0) { false }
+      mock(database).complete_todo_item(0, true) {true}
+      mock(social_network).spam(description + social_sufix) {true}
+
+      list << item
+      list.toggle_state(0)
+    end
+
+    context "with empty title of the item" do
+      let(:title)   { "" }
+
+      it "should not notify the social network if the title of the item is missing" do
+        dont_allow(social_network).spam(description)   # ALTERNATYWNE PODEJŚCIE - które lepsze?
+        #dont_allow(database).add_todo_item(item)
+
+        list << item
+      end
+    end
+
+    context "with missing description" do
+      let(:description) {nil}
+
+      it "should notify the social network if the body of the item is missing" do
+        mock(database).add_todo_item( item )  {true}
+        mock(social_network).spam(description) {true} # ALTERNATYWNE PODEJŚCIE - które lepsze?
+
+        list << item
+      end
+    end
+
+    context "with notify longer than 255 characters" do
+      let(:description)     {"There are several ways to make navigation responsive, and usually the solution we need is quite straightforward. But despite the apparent simplicity, there are many underlying factors which, when thought through and implemented properly, can make a simple solution even better without adding more complexity to the user interface.
+"}
+      it "should cut the title of the item when notifying the SN while adding an item" do
+        mock(database).add_todo_item( item )  {true}
+        mock(social_network).spam(description) {true}
+
+        list << item
+      end
+
+      it "should cut the title of the item when notifying the SN while completing the item" do
+        mock(database).add_todo_item( item )  {true}
+        mock(social_network).spam(description) {true}
+
+        list << item
+      end
+    end
+
   end
 
 
